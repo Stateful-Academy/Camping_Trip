cb08a8c2c7a541688bf627ee8c30b8f62cca077e
Karl Pfister <kpfister@me.com>
2022-04-12 11:40:32 -0600

    Updates instrcutions
    • Updates the instrcutions to be more clear
    • Adds for-each hint
    • Adds links for documentaion

--- Camping_Trip.playground/Pages/Final Steps.xcplaygroundpage/Contents.swift
+++ Camping_Trip.playground/Pages/Final Steps.xcplaygroundpage/Contents.swift
@@ -1,33 +1,52 @@
 //: [Previous](@previous)
 
 /*:
- #Final Steps
-  
-  The `Memory Maker 2022` camping trip is just about to make way! All we have left to do is choose our destination, verify we have the items we need, and then get going!
-  
-  * To choose our destination we need to define the possibilities. Create four constants with the values
-  "Warm, and sunny. Good fishing"
-  "Rainy, but with lots of trees"
-  "High chance of snow, but amazing views"
-  "High temperatures with no shade. The rockclimbing is top tier"
-  
-  * Choose one of the four constants you just created as your personal preferred `destination`
-  * As the group discusses where to go you'll want to share your opinion on the matter. Create an `if - else` statement that checks if `destination` matches each of the four options. Create a unique print statement for each case.
-  * Now that you've written that long `if - else` statement you notice it's a bit large... Create a `switch` statement to handle the same logic. The `Default` would be to just stay home.
-  
-  * We finally know where we are going! Let's do a final check on our equipment. Create a `backpack` array and assign it the value of four Strings. "Tent", "Jacket", "Toothbrush","socks"
-  * Let's look at each item in the backpack to ensure we have it all. Write a `for-each` statement that prints each item in your backpack.
-  * let's repack that bag and get on our way! The camp spot is only `20` miles away now! Create a variable named `distanceToCamp` and set it to `20`
-  * Everyone is a bit tired from all the shopping and math so some of your friends have decided to ask you if we are there yet every 5 miles. Write a while loop that will print "Are we there yet?" If the `distanceToCamp` is greater than `0`. Be sure to remove `5` each iteration.
-  
-  * WE MADE IT! WE ARE FINALLY AT BASE CAMP! We need to get the tent out of the `backpack`
-  * Declare a function named `lookForItem` with a parameter of an item: String
-  * Write a for loop the checks if the `currentItem` is equal to the `item` you are looking for. If you found it print "Nice! I found the \(item)" and break out of this `for loop`. Otherwise, print "Keep looking... I swear it's in there"
-  * Be sure to call this function and look for the tent.
-  
-  Nice work! You've worked hard for this trip! Enjoy some good times with your friends!
-  
-  Great work!
+ ### Final Steps
+
+ The `Memory Maker 2022` camping trip is just about to make way! All we have left to do is choose our destination, verify we have the items we need, and then get going!
+
+ 1. To choose our destination, we need to define the possibilities. Create four `constants` with the values
+     1.  “Warm and sunny. Good fishing”
+     2.  “Rainy, but with lots of trees.”
+     3.  “High chance of snow, but amazing views”
+     4.  “High temperatures with no shade. The rock-climbing is top tier.”
+ 2.  Choose one of the four constants you just created as your preferred personal `destination`
+ 3. As the group discusses where to go, you’ll want to share your opinion on the matter. Create an `if - else` statement that checks if `destination` matches each of the four options.
+     1. Create a unique print statement for each case.
+ 4. Now that you’ve written that long `if - else` statement, you notice it’s a bit large… Create a `switch` statement to handle the same logic.
+     1. You can use the documentation via this [link](https://docs.swift.org/swift-book/LanguageGuide/ControlFlow.html) to learn more about `switch` statements.
+     2. Each `switch` needs a `defualt` case. The `default` would be just to stay home.
+ 5. We finally know where we are going! Let’s do a final check on our equipment. Create a `backpack` array and assign a default value of four Strings. `“Tent”,“Jacket”, “Toothbrush”,”socks”`
+ 6. Let’s look at each item in the backpack to ensure we have it all. Write a `for-each` statement that prints each item in your backpack.
+     1. You can use the documentation via this [link](https://docs.swift.org/swift-book/LanguageGuide/ControlFlow.html) to learn more about `for-each` statement
+     - Hint: For-Each
+         
+         ```swift
+         let numberWords = ["one", "two", "three"]
+         for word in numberWords {
+             print(word)
+         }
+         // Prints "one"
+         // Prints "two"
+         // Prints "three"
+         
+         numberWords.forEach { word in
+             print(word)
+         }
+         // Same as above
+         ```
+         
+ 7.  Let’s repack that bag and get on our way! The camp spot is only `20` miles away now! Create a `variable` named `distanceToCamp` and set it to `20`
+ 8. Everyone is a bit tired from all the shopping and math, so some of your friends have decided to ask you if we are there yet every `5` miles. Write a while loop that will print `“Are we there yet?”` If the `distanceToCamp` is greater than `0`.
+     1. Be sure to remove `5` each iteration.
+ 9. WE MADE IT! WE ARE FINALLY AT BASE CAMP! We need to get the tent out of the `backpack`. Declare a function named `lookForItem` with a `parameter` of an `item: String`
+ 10. Write a `for loop` that checks if the `currentItem` is equal to the `item` you are looking for.
+     1. If you found it print “Nice! I found the item” and `break` out of this `for loop`.
+         1. Be sure to use `string interpolation`.
+     2. Otherwise, print “Keep looking… I swear it’s in there.”
+ 11. Be sure to `call` this function and look for the tent.
+
+ Nice work! You’ve worked hard for this trip! Enjoy some good times with your friends!
  
  */
 /*:
